20c135001f35deee68b212533458a8ad99d3aadf
Endel Dreyer <endel.dreyer@gmail.com>
2016-12-10 11:23:06 +0100

    fix protocol when running locally

--- client/src/game/network.ts
+++ client/src/game/network.ts
@@ -4,7 +4,7 @@ const PROTOCOL = window.location.protocol.replace("http", "ws");
 
 const ENDPOINT = (window.location.hostname.indexOf("heroku") >= 0)
     ? `${ PROTOCOL }//${ window.location.hostname }` // port 80 on heroku
-    : `${ PROTOCOL }://${ window.location.hostname }:2657` // port 2657 on localhost
+    : `${ PROTOCOL }//${ window.location.hostname }:2657` // port 2657 on localhost
 
 export const client = new Client(ENDPOINT);
 export const room = client.join("game");
